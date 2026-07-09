b1dca3a136fc9dbbd7690f2b642eacd8611fcf4b
Hayden Reisfelt <reisfha@icloud.com>
2026-07-09 14:16:14 -0700

    TechnoRouge Windows portable build
    Extracted Electron + game files for Windows x64.
    Run electron.exe to play.

---
+++ README.md
@@ -0,0 +1,5 @@
+# TechnoRouge
+
+A hyper-futuristic roguelike dungeon crawler built with Three.js.
+
+Run `electron.exe` to play.
---
+++ resources/app/electron/main.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    resizable: true,
+    fullscreen: false,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  const isDev = !app.isPackaged;
+
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:5173');
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
